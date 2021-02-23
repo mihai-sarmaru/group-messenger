ed575f7b0060045095342f01d02405ebd258214a
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-02-23 17:21:43 +0200

    Login with google provider

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './Login.css';
 import { Button } from '@material-ui/core';
 
-const Login = () => {
+interface LoginProps {
+    onLoginClick: () => void;
+}
+
+const Login:React.FC<LoginProps> = (props) => {
     return (
         <div className='login'>
             <div className="login__container">
                 <img src='/icons/chat-icon.png' alt='Group Messenger' />
                 <h1>Group Messenger</h1>
-                <Button color='primary' variant='outlined'>Login with Google</Button>
+                <Button color='primary' variant='outlined'
+                    onClick={props.onLoginClick}>Login with Google</Button>
             </div>
         </div>
     )
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,5 +3,9 @@ import { firebaseConfig } from './firebaseConfig';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider }
 
 export default db;
