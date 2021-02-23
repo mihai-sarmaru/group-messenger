3eac99899bef54d7c567894b52768a05552cd8b2
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-02-23 16:14:23 +0200

    Add proper firebase interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,4 +1,24 @@
+import firebase from 'firebase';
+
 export interface IRoom {
     id?: string;
     name: string;
+}
+
+export interface IFirebaseRoom {
+    id?: string;
+    data: firebase.firestore.DocumentData;
+}
+
+export interface ChatParamTypes {
+    roomId?: string;
+}
+
+export interface IMessage {
+    uid: string;
+    name: string;
+    email: string;
+    avatar: string;
+    message: string;
+    timestamp: firebase.firestore.FieldValue;
 }
