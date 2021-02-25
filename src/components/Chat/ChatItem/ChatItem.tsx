ad960d5dfd4c74fa9650162e08f4b09798300908
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-02-25 16:26:20 +0200

    Add converted server timestamp to date and time

--- src/components/Chat/ChatItem/ChatItem.tsx
+++ src/components/Chat/ChatItem/ChatItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './ChatItem.css';
+import firebase from 'firebase';
 import { Avatar } from '@material-ui/core';
 import { IMessage } from '../../../utils/interfaces';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../../store/rootStore';
+import { convertDate } from '../../../utils/dateConverter';
 
 interface ChatItemProps {
     message: IMessage;
@@ -17,7 +19,10 @@ const ChatItem:React.FC<ChatItemProps> = (props) => {
         <div className={`chatItem ${user.uid === props.message.user.uid ? 'chatItem-user' : ''}`}>
             <Avatar src={props.message.user.avatar} alt={props.message.user.name} />
             <div className="chatItem__message">
-                <h6>{props.message.user.name}</h6>
+                <div className="chatItem__messageInfo">
+                    <h6>{props.message.user.name}</h6>
+                    <p>{props.message.timestamp && convertDate((props.message.timestamp as firebase.firestore.Timestamp).toMillis())}</p>
+                </div>
                 <p>{props.message.message}</p>
             </div>
         </div>
